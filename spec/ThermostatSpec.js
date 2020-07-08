21f724af1a333b5b45ebfcdb844435d0fb05747f
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-07-08 16:07:32 +0100

    Thermostat challenge: thermostat activated with ability to increase and decrease temperature, with a minimum temperature of 10 degrees set. Tests pass.

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,25 @@
+'use strict';
+describe ('Thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();  
+  })
+  it('should have a starting temp of 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+  it('increases in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+  it('decreases in temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+});
