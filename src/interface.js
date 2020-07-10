edc32d2048834769c084b56ce5ca744a172c0767
India Rex <face940@gmail.com>
2020-07-10 14:56:50 +0100

    power saving mode buttons now work

--- src/interface.js
+++ src/interface.js
@@ -17,9 +17,14 @@ $(document).ready(function() {
     updateTemperature();
   });
 
-  $('#temperature-reset').click(function() {
-    thermostat.resetTemperature();
-    updateTemperature();
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off');
+  });
+
+  $('#powersaving-on').click(function () {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on');
   });
 
   
